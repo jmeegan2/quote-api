ec70f2956e2ec7f130b6bb21ce9c4c8336ac7d79
James Meegan <jmeegan2@owlmail.harford.edu>
2022-01-31 22:33:34 -0500

    Got quote to appear with quotes around

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 
     fetch('https://api.kanye.rest')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+    console.log(data)
+    displayQuote(data)
+  });
+
+
+  function displayQuote(data) {
+    const quote = data.quote;
+    const quoteDiv = document.getElementById("quote");
+  
+
+  const quoteName = '"'+quote+'"';
+  const heading = document.createElement("p");
+
+  heading.innerHTML = quoteName;
+  quoteDiv.appendChild(heading);
+  }
+//   document.getElementById('data').innerHTML
+
+ 
