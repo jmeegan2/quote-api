fc5f8a6c55cefc542f65ac595ed5438990b4cfa1
jmeegan2 <jmeegan8@gmail.com>
2023-01-23 20:55:45 -0500

    init

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 
-    fetch('https://api.kanye.rest')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-    displayQuote(data)
-  });
+//     fetch('https://api.kanye.rest')
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data)
+//     displayQuote(data)
+//   });
 
 
-  function displayQuote(data) {
-    const quote = data.quote;
-    const quoteDiv = document.getElementById("quote");
+//   function displayQuote(data) {
+//     const quote = data.quote;
+//     const quoteDiv = document.getElementById("quote");
   
 
-  const quoteName = '"'+quote+'"';
-  const heading = document.createElement("p");
+//   const quoteName = '"'+quote+'"';
+//   const heading = document.createElement("p");
 
-  heading.innerHTML = quoteName;
-  quoteDiv.appendChild(heading);
-  }
-//   document.getElementById('data').innerHTML
+//   heading.innerHTML = quoteName;
+//   quoteDiv.appendChild(heading);
+//   }
+// //   document.getElementById('data').innerHTML
 
  
+
+// fetch('https://zenquotes.io/api/quotes/')
+// .then(response => response.json())
+// .then(data => {
+//   console.log(data)
+//   displayQuote(data)
+// });
+
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
+
+fetch("https://zenquotes.io/api/quotes/", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
